c463c20e9d68db232c4107d579eb3749ce710ccf
ThomasGilman <Thomas.Gilman@ymail.com>
2019-08-30 19:54:22 -0400

    python basic bot works

--- ChatBot.py
+++ ChatBot.py
@@ -1,4 +1,12 @@
-from socket import *
+import re
+import socket
+import time
+
+HOST = "irc.chat.twitch.tv"
+PORT = 6667
+OAUTH = "w74fi7jcegv41r2b2x25ktidpyb7vq"
+USERNAME = "bFriend"
+CHANNEL = "nilkin1"
 
 class chatConnection:
     HOST = None
@@ -6,6 +14,8 @@ class chatConnection:
     OAUTH = None
     BOT_USERNAME = None
     CHANNEL = None
+    MSG_PATTERN = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
     def __init__(self, host, port, ps, nick, join):
         self.HOST = host
         self.PORT = port
@@ -13,15 +23,71 @@ class chatConnection:
         self.BOT_USERNAME = nick
         self.CHANNEL = join
 
+class bot:
+    botSocket = None
+    cfg = None
+    def __init__(self, cfg):
+        # create socket
+        self.botSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.cfg = cfg
+
+    def sendMsg(self, msg):
+        self.botSocket.send((msg+"\r\n").encode('utf-8'))
+
+    def recv(self):
+        return self.botSocket.recv(2048).decode("utf-8")
+
+    def keepAlive(self):
+        self.sendMsg("PONG :tmi.twitch.tv")
+
+    def connect(self):
+        # attempt connection
+        try:
+            self.botSocket.connect((self.cfg.HOST, self.cfg.PORT));
+            self.sendMsg("PASS oauth:{0}".format(self.cfg.OAUTH))
+            self.sendMsg("NICK {0}".format(self.cfg.BOT_USERNAME))
+            self.sendMsg("JOIN #{0}".format(self.cfg.CHANNEL))
+            print("connected")
+            return True
+        except Exception as e:
+            print("failed to connect!!! {0}".format(e))
+            return False
+
+    def announceArrival(self):
+        self.sendMsg("PRIVMSG #{0} :{1} has arrived!!!".format(self.cfg.CHANNEL, self.cfg.BOT_USERNAME))
+        self.sendMsg("/mod {0}".format(self.cfg.CHANNEL))
+
+    def channelResponse(self, msg):
+        self.sendMsg("PRIVMSG #{0} :{1}".format(self.cfg.CHANNEL, msg))
 
 def main():
-
-    localHost = 127001;
-    cfg = chatConnection()
+    cfg = chatConnection(HOST, PORT, OAUTH, USERNAME, CHANNEL)
 
     try:
-        botSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        botSocket.connect(
+        #create bot and attempt connection
+        botSocket = bot(cfg)
+        connected = botSocket.connect()
+
+        while connected:
+            response = botSocket.recv()
+            print(response)
+            if response.startswith("PING"):
+                botSocket.keepAlive()
+            else:
+                #Get message passed from users
+                user = re.search(r"\w+", response).group(0)
+                message = cfg.MSG_PATTERN.sub("", response).strip()
+
+                print("{0}: {1}".format(user, message))
+                if(message.startswith("$Hello")):
+                    botSocket.channelResponse("HI!!!")
+                #elif(message.startswith("$ColorTxt")):
+                #    botSocket.sendMsg("/me this is color text!!!")
+                #elif(message.startswith("$ByeFriend")):
+                #   botSocket.sendMsg("/disconnect")
+            time.sleep(1)
 
     finally:
         print("exiting")
+
+main()
--- lab 1/ChatBot/ChatBot/Program.cs
+++ lab 1/ChatBot/ChatBot/Program.cs
@@ -21,100 +21,8 @@ public struct serverCred
 
 class Program
 {
-    //TRY USING IRC
-    public string msgEnd = "\\r\\n";
-    /*public static void ConnectSocket(Socket s, serverCred cfg)
-    {
-        IPHostEntry hostEntry = null;
-
-        hostEntry = Dns.GetHostEntry(cfg.HOST);
-        foreach (IPAddress address in hostEntry.AddressList)
-        {
-            IPEndPoint iPEnd = new IPEndPoint(address, cfg.PORT);
-            Console.WriteLine("Address:{0}\nPort:{1}", iPEnd.Address.ToString(), iPEnd.Port.ToString());
-            Socket tempS
-                = new Socket(iPEnd.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            if (tempS.Connected)
-            {
-                s = tempS;
-                break;
-            }
-        }
-        if (s == null)
-            Console.WriteLine("failed to connect");
-    }
-
-    public static void sendMsg(Socket s, string msg)
-    {
-        s.Send(encodeString(msg));
-    }
-
-    private static byte[] encodeString(string s)
-    {
-        return Encoding.UTF8.GetBytes(s);
-    }
-
-    private static string decodeMsg(Decoder decoder, byte[] msgData)
-    {
-        int charCount = decoder.GetCharCount(msgData, 0, msgData.Length);
-        Char[] chars = new Char[charCount];
-        decoder.GetChars(msgData, 0, charCount, chars, 0);
-
-        return chars.ToString();
-    }
-
-    private void connectToHost(Socket s, serverCred cfg)
-    {
-        string Pass = "PASS " + cfg.OAUTH + msgEnd;
-        string Nick = "NICK " + cfg.BOT_USERNAME + msgEnd;
-        string Join = "JOIN " + cfg.CHANNEL + msgEnd;
-
-        sendMsg(s, Pass);
-        sendMsg(s, Nick);
-        sendMsg(s, Join);
-    }
-
-    public static Socket botSocket = null;
-    public static serverCred cfg;
-    public static byte[] buffer = new byte[2048];
-    public static Decoder utf8Decoder;
-    static void Main(string[] args)
-    {
-        cfg = new serverCred();
-        cfg.HOST            = "irc.chat.twitch.tv";
-        cfg.PORT            = 6667;
-        cfg.OAUTH           = "w74fi7jcegv41r2b2x25ktidpyb7vq";
-        cfg.BOT_USERNAME    = "bFriend";
-        cfg.CHANNEL         = "nilkin1";
-
-        Console.WriteLine(cfg);
-
-        ConnectSocket(botSocket, cfg);
-        utf8Decoder = Encoding.UTF8.GetDecoder();
-
-        if (botSocket != null && botSocket.Connected)
-        {
-            Console.WriteLine("Connected to server");
-            while (botSocket.Connected)
-            {
-                botSocket.Listen(1);
-                botSocket.Receive(buffer);
-                string msg = decodeMsg(utf8Decoder, buffer);
-                if (msg == "PING")
-                {
-                    sendMsg(botSocket, "PONG");
-                }
-                else
-                {
-                    Console.WriteLine("got : {0}", msg);
-                }
-            }
-        }
-        else
-            Console.WriteLine("Couldnt Connect or lost Connection");
-
-        Console.WriteLine("Fin");
-    }*/
+    
+    public static string msgEnd = "\r\n";
 
     public class botClient
     {
@@ -131,15 +39,18 @@ class Program
             this.Channel = channel;
 
             tcpClient = new TcpClient(ip, port);
-            input = new StreamReader(tcpClient.GetStream(), Encoding.UTF8);
-            output = new StreamWriter(tcpClient.GetStream(), Encoding.UTF8);
+            input = new StreamReader(tcpClient.GetStream());//, Encoding.UTF8);
+            output = new StreamWriter(tcpClient.GetStream());//, Encoding.UTF8);
 
             try
             {
-                sendMessage("PASS {0}\r\n", passwrd);
-                sendMessage("NICK {0}\r\n", userName);
-                sendMessage("JOIN {0}\r\n", Channel);
-                output.Flush();
+
+                sendMessage("PASS oauth: {0}", passwrd);
+                sendMessage("NICK {0}", userName);
+                sendMessage("JOIN #{0}", Channel);
+
+                if(tcpClient.Connected)
+                    Console.WriteLine("Connected");
             }
             catch (Exception e)
             {
@@ -156,7 +67,7 @@ class Program
         {
             try
             {
-                output.WriteLine(format, arg);
+                output.WriteLine(format+msgEnd, arg);
                 output.Flush();
             }
             catch(Exception e)
@@ -177,7 +88,7 @@ class Program
         }
         public void pingResponse()
         {
-            sendMessage("PONG\r\n");
+            sendMessage("PONG :tmi.twitch.tv");
         }
     }
 
@@ -192,17 +103,18 @@ class Program
         cfg.CHANNEL = "nilkin1";
 
         botClient bFriend = new botClient(cfg.BOT_USERNAME, cfg.OAUTH, cfg.HOST, cfg.CHANNEL, cfg.PORT);
-        string incomingMsg = "";
 
         while(bFriend.getConnected())
         {
             try
             {
-                Console.WriteLine("Connected");
-                incomingMsg = bFriend.recieveMessage();
-                Console.WriteLine("Got: {0}", incomingMsg);
-                if (incomingMsg.StartsWith("PING"))
-                    bFriend.pingResponse();
+                string incomingMsg = bFriend.recieveMessage();
+                if(incomingMsg != null)
+                {
+                    Console.WriteLine("Got: {0}", incomingMsg);
+                    if (incomingMsg.StartsWith("PING"))
+                        bFriend.pingResponse();
+                }
             }
             catch(Exception e)
             {
